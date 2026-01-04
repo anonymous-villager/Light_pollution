5ad87378845caa5822ea0bb4a4fc1011a7163490
anonymous-villager <villiager9487@gmail.com>
2026-01-04 12:41:15 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Light-pollution/',
+  base: '/Light_pollution/',
   plugins: [react()],
   server: {
     proxy: {
